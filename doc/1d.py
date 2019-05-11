8c22be563de9f0e2923de96c03a3ae25cadaea21
changhoonhahn <changhoonhahn@lbl.gov>
2019-05-10 23:53:35 -0700

    implemented 1D ADCG and also with sparse prior loss function

--- doc/1d.py
+++ doc/1d.py
@@ -4,9 +4,10 @@ simple 1d localization problem
 
 '''
 import os 
-import scipy.optimize
 import autograd as Agrad
 import autograd.numpy as np 
+import scipy.optimize
+from scipy.stats import norm as Norm
 # --- locahbay
 from locahbay import util as UT 
 # -- plotting -- 
@@ -25,8 +26,8 @@ mpl.rcParams['ytick.major.width'] = 1.5
 mpl.rcParams['legend.frameon'] = False
 
 
-xpix = np.linspace(0., 1., 50) # default pixel gridding 
-sig_psf = 0.1 # psf width
+xpix = np.linspace(0., 1., 200) # default pixel gridding 
+sig_psf = 0.02 # psf width
 sig_nois = 0.1 # noise level 
 
 def psi(theta): 
@@ -35,7 +36,7 @@ def psi(theta):
     '''
     return np.exp(-((xpix - theta)/sig_psf)**2)
 
-theta_grid = np.linspace(0., 1., 30) 
+theta_grid = np.linspace(0., 1., len(xpix)) 
 grid_psi = np.stack([psi(tt) for tt in theta_grid])  
 
 def Psi(ws, thetas): 
@@ -82,6 +83,42 @@ def ell(ws, thetas, yobs):
     return ((Psi(ws, thetas) - yobs)**2).sum() 
 
 
+def ell_noise(ws, thetas, yobs): 
+    ''' loss function  = likelihood 
+    '''
+    return ((Psi(ws, thetas) - yobs)**2/sig_nois**2).sum() 
+
+
+def ell_sparseprior(ws, thetas, yobs, **kwargs): 
+    ''' loss function with a sparse prior 
+    '''
+    f_density = kwargs['f_density']
+    lnlike = ((Psi(ws, thetas) - yobs)**2/sig_nois**2).sum() 
+
+    def p1(_I): # the intensity function is uniform between 1 and 2 
+        _I = np.atleast_1d(_I)
+        _p1 = np.zeros_like(_I) 
+        _p1[(_I >= 1.) & (_I <= 2.)] = 1.
+        return _p1
+
+    #def p2(_I): 
+    #    _p2 = lambda ii: p1(_I - ii) * p1(ii) 
+    #    return sp.integrate.quad(_p2, -np.inf, np.inf)[0]
+
+    def lnPrior(ws): 
+        wlim = (ws > 0.)
+        _p1 = np.array([p1(w) for w in ws[wlim]])
+        #p2tt = np.array([p2(ttt) for ttt in tt])
+        # ignore second order term for now 
+        pri = np.sum(np.log((1.-f_density) * np.clip(Norm.pdf(np.log(ws[wlim]), loc=-10, scale=1), 1e-5, None)/ws[wlim] + f_density * _p1)) 
+        if np.isfinite(pri): 
+            return -pri
+        else: 
+            raise ValueError
+    lnprior = lnPrior(ws) 
+    return lnlike + lnprior
+
+
 def lmo(v): 
     ''' "linear maximization oracle"
     This function does the following optimization 
@@ -96,7 +133,7 @@ def lmo(v):
     return theta_grid[ip.argmin()] 
 
 
-def coordinate_descent(thetas, yobs, iter=35, min_drop=1e-5):  
+def coordinate_descent(thetas, yobs, lossFn, iter=35, min_drop=1e-5, **lossfn_kwargs):  
     ''' block coordinate descent algorithm. 
     compute weights, prune support, locally improve support
     '''
@@ -105,7 +142,7 @@ def coordinate_descent(thetas, yobs, iter=35, min_drop=1e-5):
         return scipy.optimize.nnls(np.stack([psi(tt) for tt in thetas]).T, yobs)[0]
     def min_thetas(): 
         res =  scipy.optimize.minimize(
-                Agrad.value_and_grad(lambda tts: ell(ws, tts, yobs)), thetas, 
+                Agrad.value_and_grad(lambda tts: lossFn(ws, tts, yobs, **lossfn_kwargs)), thetas, 
                 jac=True, method='L-BFGS-B', bounds=[(0.0, 1.0)]*len(thetas))
         return res['x'], res['fun']
     
@@ -119,7 +156,7 @@ def coordinate_descent(thetas, yobs, iter=35, min_drop=1e-5):
     return ws, thetas 
 
 
-def adcg(yobs, local_update, max_iters): 
+def adcg(yobs, lossFn, local_update, max_iters, **lossfn_kwargs): 
     ''' Alternative Descent Conditional Gradient method implementation 
     carefully following Nick Boyd's tutorial. Mainly involves two optimization 
     problems
@@ -129,12 +166,13 @@ def adcg(yobs, local_update, max_iters):
     history = [] 
     for i in range(max_iters): 
         residual = output - yobs
-        loss = (residual**2).sum() 
+        #loss = (residual**2).sum() 
+        loss = lossFn(ws, thetas, yobs, **lossfn_kwargs) 
         print('iter=%i, loss=%f' % (i, loss)) 
         history.append((loss, ws, thetas))
 
         theta = lmo(residual) 
-        ws, thetas = local_update(np.append(thetas, theta), yobs)
+        ws, thetas = local_update(np.append(thetas, theta), yobs, lossFn, **lossfn_kwargs)
         output = Psi(ws, thetas)
     return history 
 
@@ -145,11 +183,25 @@ def select_k(history):
     return history[k_hat]
 
 
-def runADCG(): 
-    fobs = os.path.join(UT.dat_dir(), 'obs1d.psf%s.nois%s.Nsource%i.seed%i.npy' % 
-            (str(sig_psf), str(sig_nois), 5, 1))
-    yobs = np.load(fobs) 
-    hist = adcg(yobs, coordinate_descent, 10)
+def ADCG_1d(loss_str='l2', N_source=3, seed=1, **lossfn_kwargs): 
+    '''
+    '''
+    np.random.seed(seed)
+    thetas_true = np.random.rand(N_source) # x_true positions 
+    weights_true = 2.*np.random.rand(N_source) # weights --- in SMLM intensities 0 to 1 
+    print thetas_true
+
+    yobs = Psi(weights_true, thetas_true) + sig_nois * np.random.randn(len(xpix)) 
+
+    #fobs = os.path.join(UT.dat_dir(), 'obs1d.psf%s.nois%s.Nsource%i.seed%i.npy' % 
+    #        (str(sig_psf), str(sig_nois), 5, 1))
+    #yobs = np.load(fobs) 
+    if loss_str is 'l2': 
+        hist = adcg(yobs, ell, coordinate_descent, 10)
+    elif loss_str == 'l2_noise':
+        hist = adcg(yobs, ell_noise, coordinate_descent, 30)
+    elif loss_str == 'l2_noise_sparse': 
+        hist = adcg(yobs, ell_sparseprior, coordinate_descent, 30, **lossfn_kwargs)
     loss, ws, thetas = select_k(hist)
     output = Psi(ws, thetas) 
 
@@ -157,21 +209,25 @@ def runADCG():
     fig = plt.figure(figsize=(10,5))
     sub = fig.add_subplot(111)
     sub.scatter(xpix, yobs, marker='x', s=10) 
+    for i in range(len(thetas_true)):
+        sub.plot(xpix, weights_true[i] * psi(thetas_true[i]), c='k', ls=':', lw=0.5)
+
     sub.plot(xpix, output, c='k') 
     for i in range(len(thetas)): 
         sub.plot([thetas[i], thetas[i]], [0., 2.5], c='k', ls='--') 
-    loss, ws, thetas = hist[-1]
-    for i in range(len(thetas)): 
-        sub.plot([thetas[i], thetas[i]], [0., 2.5], c='C0', ls='--') 
+        #sub.plot(xpix, ws[i] * psi(thetas[i]), c='k', ls=':', lw=0.5)
     sub.set_xlabel(r'$x_{\rm pix}$', fontsize=25) 
     sub.set_xlim(0., 1.) 
     sub.set_ylabel('intensity', fontsize=25) 
     sub.set_ylim(0., 2.5) 
-    fig.savefig(os.path.join(UT.fig_dir(), os.path.basename(fobs).replace('.npy', '.adcg.png')), bbox_inches='tight')
+    fobs = os.path.join(UT.dat_dir(), 'obs1d.psf%s.nois%s.Nsource%i.seed%i.npy' % 
+            (str(sig_psf), str(sig_nois), N_source, seed))
+    fig.savefig(os.path.join(UT.fig_dir(), os.path.basename(fobs).replace('.npy', '.%s.adcg.png' % loss_str)), 
+            bbox_inches='tight')
     return None 
 
 
-
 if __name__=="__main__": 
-    #obs1d(N_source=5, seed=1)
-    runADCG()
+    ADCG_1d(loss_str='l2')
+    ADCG_1d(loss_str='l2_noise')
+    ADCG_1d(loss_str='l2_noise_sparse', f_density=3./200.)
